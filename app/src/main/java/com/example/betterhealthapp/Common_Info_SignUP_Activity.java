7f88ff5fedd95f0a8cc1dbb8974767ec5577e00a
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-04-21 00:55:53 +0200

    Base One

--- app/src/main/java/com/example/betterhealthapp/Common_Info_SignUP_Activity.java
+++ app/src/main/java/com/example/betterhealthapp/Common_Info_SignUP_Activity.java
@@ -1,4 +1,4 @@
-package com.example.betterhealthapp.paramedic;
+package com.example.betterhealthapp;
 
 import android.os.Bundle;
 
@@ -8,15 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.betterhealthapp.R;
-
-public class Paramedic_Dashboard_Activity extends AppCompatActivity {
+public class Common_Info_SignUP_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_paramedic_dashboard);
+        setContentView(R.layout.activity_common_info_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/betterhealthapp/Sign_In_Activity.java
+++ app/src/main/java/com/example/betterhealthapp/Sign_In_Activity.java
@@ -1,4 +1,4 @@
-package com.example.betterhealthapp.student;
+package com.example.betterhealthapp;
 
 import android.os.Bundle;
 
@@ -8,15 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.betterhealthapp.R;
-
-public class Student_Dashboard_Activity extends AppCompatActivity {
+public class Sign_In_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_student_dashboard);
+        setContentView(R.layout.activity_sign_in);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
