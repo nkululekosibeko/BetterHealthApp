255821ad734baf3f7053ae249717cbea78af4b1d
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-04-21 14:56:38 +0200

    Extras after start

--- app/src/main/java/com/example/betterhealthapp/MainActivity.java
+++ app/src/main/java/com/example/betterhealthapp/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
